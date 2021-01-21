c8579c3825e088955f4eb559365ee14e64b1753e
spencermynatt <75866352+spencermynatt@users.noreply.github.com>
2021-01-21 11:55:32 -0600

    Update spencer's triangle.cpp

--- spencer's triangle.cpp
+++ spencer's triangle.cpp
@@ -6,7 +6,7 @@ const char* vertex_shader =
 "layout (location = 0) in vec3 position;\n"
 "void main()\n"
 "{\n"
-"   gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+"   gl_Position = vec4(position.x, position.y, position.z, 1.0);\n" // homogeneous vertex coordinate
 "}\0";
 const char* fragment_shader =
 "#version 430\n"
